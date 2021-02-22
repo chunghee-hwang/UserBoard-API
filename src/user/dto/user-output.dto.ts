b70510dffed97b7487244b45c2c3bbc17a026eda
CH Hwang <hch0821@naver.com>
2021-02-23 06:59:15 +0900

    Update: extract user-output.dto from create-account.dto

---
+++ src/user/dto/user-output.dto.ts
@@ -0,0 +1,13 @@
+import {
+  ObjectType,
+  PartialType,
+  PickType,
+  IntersectionType,
+} from '@nestjs/graphql';
+import { BaseOutput } from 'src/shared/dto/base.output.dto';
+import { User } from '../user.model';
+
+@ObjectType()
+export class UserOutput extends PartialType(
+  PickType(IntersectionType(User, BaseOutput), ['id', 'name', 'ok', 'error']),
+) {}

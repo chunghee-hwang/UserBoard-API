c75854f20c976cb14eb978455e593111127e5bde
CH Hwang <hch0821@naver.com>
2021-02-23 08:21:05 +0900

    Delete: not needed hello() resolver & service

--- src/app/app.resolver.ts
+++ src/app/app.resolver.ts
@@ -5,10 +5,6 @@ import { SearchUserOrBoardsOutput } from './dto/search-user-or-boards.dto';
 @Resolver((_) => String)
 export class AppResolver {
   constructor(private readonly _appService: AppService) {}
-  @Query((_) => String)
-  hello(@Args('data', { type: () => String, nullable: true }) data?: string) {
-    return this._appService.getHello(data);
-  }
 
   // 유저 아이디로 유저 정보나 유저가 작성한 게시물 검색
   @Query((_) => SearchUserOrBoardsOutput)

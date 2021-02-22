3f7bef62bd86dfa069fdbfe7253b1d5459612772
CH Hwang <hch0821@naver.com>
2021-02-23 07:46:11 +0900

    Update: Output type of search-user-or-boards.dto

--- src/app/dto/search-user-or-boards.dto.ts
+++ src/app/dto/search-user-or-boards.dto.ts
@@ -1,12 +1,15 @@
-import { Field, ObjectType } from '@nestjs/graphql';
+import { Field, ObjectType, OmitType } from '@nestjs/graphql';
 import { BoardOutput } from 'src/board/dto/get-boards.dto';
 import { BaseOutput } from 'src/shared/dto/base.output.dto';
 import { UserOutput } from 'src/user/dto/user-output.dto';
 
 @ObjectType()
+export class SearchUserOutput extends OmitType(UserOutput, ['ok', 'error']) {}
+
+@ObjectType()
 export class SearchUserOrBoardsOutput extends BaseOutput {
-  @Field((_) => UserOutput, { nullable: true })
-  user?: UserOutput;
+  @Field((_) => SearchUserOutput, { nullable: true })
+  user?: SearchUserOutput;
 
   @Field((_) => [BoardOutput], { nullable: true })
   boards?: BoardOutput[];

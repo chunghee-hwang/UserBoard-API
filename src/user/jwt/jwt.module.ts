de372bf38e4bacc963b5ab44ca0576c35c12cf9d
CH Hwang <hch0821@naver.com>
2021-02-23 07:25:34 +0900

    Update: make JwtModuleglobal

--- src/user/jwt/jwt.module.ts
+++ src/user/jwt/jwt.module.ts
@@ -1,8 +1,9 @@
-import { DynamicModule, Module } from '@nestjs/common';
+import { DynamicModule, Global, Module } from '@nestjs/common';
 import { CONFIG_OPTION } from './jwt.constants';
 import { JwtModuleOptions } from './jwt.interfaces';
 import { JwtService } from './jwt.service';
 
+@Global()
 @Module({})
 export class JwtModule {
   static forRoot(options: JwtModuleOptions): DynamicModule {

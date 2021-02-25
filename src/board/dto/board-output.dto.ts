a09c56773c0cf94faf69c104ab35c8912cdc4e4c
CH Hwang <hch0821@naver.com>
2021-02-25 19:45:43 +0900

    Update output dto for preventing exposing password in board domain

--- src/board/dto/board-output.dto.ts
+++ src/board/dto/board-output.dto.ts
@@ -1,7 +1,9 @@
-import { ObjectType, OmitType } from '@nestjs/graphql';
+import { Field, IntersectionType, ObjectType, OmitType } from '@nestjs/graphql';
+import { UserOutput } from 'src/user/dto/user-output.dto';
 import { Board } from '../board.model';
 
 @ObjectType()
 export class BoardOutput extends OmitType(Board, ['author']) {
-  author: string;
+  @Field((_) => UserOutput)
+  author: UserOutput;
 }
--- src/board/dto/get-boards.dto.ts
+++ src/board/dto/get-boards.dto.ts
@@ -1,10 +1,10 @@
 // 여러 게시물 가져오기 dto
 
-import { Field, ObjectType, PickType } from '@nestjs/graphql';
+import { Field, ObjectType } from '@nestjs/graphql';
 import { BaseOutput } from 'src/shared/dto/base.output.dto';
-import { Board } from '../board.model';
+import { BoardOutput } from './board-output.dto';
 @ObjectType()
 export class GetBoardsOutput extends BaseOutput {
-  @Field((_) => [Board], { nullable: true })
-  boards?: Board[];
+  @Field((_) => [BoardOutput], { nullable: true })
+  boards?: BoardOutput[];
 }

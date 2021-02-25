66f9ece566c701de79c571084aa18855b442abb5
CH Hwang <hch0821@naver.com>
2021-02-25 19:54:59 +0900

    Update: prevent expose password property from user-output.dto

--- src/user/dto/user-output.dto.ts
+++ src/user/dto/user-output.dto.ts
@@ -10,9 +10,9 @@ import { BaseOutput } from 'src/shared/dto/base.output.dto';
 import { User } from '../user.model';
 
 @ObjectType()
-export class UserOutput extends OmitType(User, ['password']) {}
+export class UserOutput extends OmitType(User, ['password', 'boards']) {}
 
 @ObjectType()
 export class UserOutputForResolver extends PartialType(
-  IntersectionType(OmitType(User, ['password']), BaseOutput),
+  IntersectionType(OmitType(User, ['password', 'boards']), BaseOutput),
 ) {}

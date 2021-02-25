45a47470e4cb2f7428160f0747b88df94cae07c7
CH Hwang <hch0821@naver.com>
2021-02-25 20:10:22 +0900

    Update: search-user-or-boards.dto to prevent exposing password from output

--- src/app/dto/search-user-or-boards.dto.ts
+++ src/app/dto/search-user-or-boards.dto.ts
@@ -1,18 +1,15 @@
 // 유저 아이디로 유저 정보나 유저가 작성한 게시물 검색 dto
 
-import { Field, ObjectType, OmitType } from '@nestjs/graphql';
-import { Board } from 'src/board/board.model';
+import { Field, ObjectType } from '@nestjs/graphql';
+import { BoardOutput } from 'src/board/dto/board-output.dto';
 import { BaseOutput } from 'src/shared/dto/base.output.dto';
-import {
-  UserOutput,
-  UserOutputForResolver,
-} from 'src/user/dto/user-output.dto';
+import { UserOutput } from 'src/user/dto/user-output.dto';
 
 @ObjectType()
 export class SearchUserOrBoardsOutput extends BaseOutput {
   @Field((_) => UserOutput, { nullable: true })
   user?: UserOutput;
 
-  @Field((_) => [Board], { nullable: true })
-  boards?: Board[];
+  @Field((_) => [BoardOutput], { nullable: true })
+  boards?: BoardOutput[];
 }

bd071a013df970680c08b99d2c2b80a30e8b4d6b
CH Hwang <hch0821@naver.com>
2021-02-25 21:42:30 +0900

    Test: all of domain

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -51,14 +51,10 @@ export class UserService {
   // 로그인
   async loginUser({ name, password }: LoginInput): Promise<LoginOutput> {
     try {
-      const user = await this._userRepository.findOne(
-        { name },
-        { select: ['password'] },
-      );
+      const user = await this._userRepository.findOne({ name });
       if (!user) {
         return { ok: false, error: 'The username or password is not correct.' };
       }
-      console.log({ user });
       const isPasswordCorrect: boolean = await user.checkPassword(password);
       if (!isPasswordCorrect) {
         return { ok: false, error: 'The username or password is not correct.' };
--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -8,8 +8,13 @@ import request from 'supertest';
 
 describe('AppController (e2e)', () => {
   let app: INestApplication;
+  let user = {
+    name: 'hakhak',
+    password: '1234',
+    token: null,
+  };
 
-  beforeEach(async () => {
+  beforeAll(async () => {
     config({ path: resolve(__dirname, `../.${process.env.NODE_ENV}.env`) });
     console.log(process.env.NODE_ENV);
     const moduleFixture: TestingModule = await Test.createTestingModule({
@@ -20,57 +25,149 @@ describe('AppController (e2e)', () => {
     await app.init();
   });
 
-  it('get hello', () => {
-    return request(app.getHttpServer())
-      .post('/graphql')
-      .send({ query: '{hello}' })
-      .expect(200)
-      .expect(({ body }) => {
-        expect(body.data.hello).toBe('Hello World!');
-      });
-  });
-
   it('user create', () => {
-    const name = 'hakhak';
-    return request(app.getHttpServer())
-      .post('/graphql')
-      .send({ query: `mutation {createUser(name: "${name}"){name}}` })
-      .expect(200)
-      .expect(({ body }) => {
-        expect(body.data.createUser.name).toBe(name);
-      });
-  });
-
-  it('user create board', () => {
-    const board = {
-      title: '학학이 소개',
-      content: '학학이는 살아있어요',
-    };
-
-    const name = 'hakhak';
-
     return request(app.getHttpServer())
       .post('/graphql')
       .send({
-        query: `mutation {createBoard(title: "${board.title}", content:"${board.content}", userName:"${name}"){content}}`,
+        query: `mutation {createUser(name: "${user.name}", password: "${user.password}"){name, ok, error}}`,
       })
       .expect(200)
       .expect(({ body }) => {
-        expect(body.data.createBoard.title).toBe(board.title);
+        if (body.data.createUser.ok) {
+          expect(body.data.createUser.name).toBe(user.name);
+        } else {
+          expect(body.data.createUser.error).toBe('The name is in use.');
+        }
       });
   });
 
-  it('boards of user', () => {
-    const name = 'hakhak';
+  describe('user can login', () => {
+    it('user incorrect login', () => {
+      return request(app.getHttpServer())
+        .post('/graphql')
+        .send({
+          query: `mutation {loginUser(name: "${user.name}wrong", password: "${user.password}wrong"){error, token}}`,
+        })
+        .expect(200)
+        .expect(({ body }) => {
+          expect(body.data.loginUser.error).not.toBeNull();
+          expect(body.data.loginUser.token).toBeNull();
+        });
+    });
 
-    return request(app.getHttpServer())
-      .post('/graphql')
-      .send({
-        query: `query {getBoards(userName:"${name}"){author {name}}}`,
-      })
-      .expect(200)
-      .expect(({ body }) => {
-        expect(body.data.getBoards).toBe(expect.arrayContaining(['author']));
+    describe('user correct login', () => {
+      it('login', () => {
+        return request(app.getHttpServer())
+          .post('/graphql')
+          .send({
+            query: `mutation {loginUser(name: "${user.name}", password: "${user.password}"){ token }}`,
+          })
+          .expect(200)
+          .expect(({ body }) => {
+            const token = body.data.loginUser.token;
+            expect(token).not.toBeNull();
+            user.token = token;
+          });
       });
+
+      // it('user delete', () => {
+      //   return request(app.getHttpServer())
+      //     .post('/graphql')
+      //     .set('token', `${user.token}`)
+      //     .send({
+      //       query: `mutation {deleteUser{name, ok, error}}`,
+      //     })
+      //     .expect(200)
+      //     .expect(({ body }) => {
+      //       if (body.data.deleteUser.ok) {
+      //         expect(body.data.deleteUser.name).toBe(user.name);
+      //       } else {
+      //         console.log(body.data.deleteUser.error);
+      //       }
+      //     });
+      // });
+
+      describe('user can manage boards', () => {
+        let board = {
+          title: '학학이 소개',
+          content: '학학이는 살아있어요',
+          id: null,
+        };
+        it('create board', () => {
+          return request(app.getHttpServer())
+            .post('/graphql')
+            .set('token', `${user.token}`)
+            .send({
+              query: `mutation {
+                createBoard(title: "${board.title}", content:"${board.content}"){
+                  id, title, content, author{name}}
+                }`,
+            })
+            .expect(200)
+            .expect(({ body }) => {
+              const boardId = body.data.createBoard.id;
+              expect(boardId).not.toBeNull();
+              expect(body.data.createBoard.title).toBe(board.title);
+              expect(body.data.createBoard.content).toBe(board.content);
+              expect(body.data.createBoard.author.name).toBe(user.name);
+              board.id = boardId;
+            });
+        });
+
+        it('modify board', () => {
+          const newBoard = {
+            ...board,
+            title: '학학이가 수정한 제목',
+            content: '학학이가 수정한 내용',
+          };
+          return request(app.getHttpServer())
+            .post('/graphql')
+            .set('token', `${user.token}`)
+            .send({
+              query: `mutation {
+                modifyBoard(title: "${newBoard.title}", content:"${newBoard.content}", id:${board.id}){
+                  title, content}
+                }`,
+            })
+            .expect(200)
+            .expect(({ body }) => {
+              expect(body.data.modifyBoard.title).toBe(newBoard.title);
+              expect(body.data.modifyBoard.content).toBe(newBoard.content);
+            });
+        });
+
+        it('delete board', () => {
+          return request(app.getHttpServer())
+            .post('/graphql')
+            .set('token', `${user.token}`)
+            .send({
+              query: `mutation {
+                deleteBoard(id:${board.id}){
+                  id }
+                }`,
+            })
+            .expect(200)
+            .expect(({ body }) => {
+              expect(body.data.deleteBoard.id).toBe(board.id);
+            });
+        });
+      });
+
+      describe('user can view boards', () => {
+        it('boards of user', () => {
+          return request(app.getHttpServer())
+            .post('/graphql')
+            .send({
+              query: `query {getBoards(userName:"${user.name}"){author {name}}}`,
+            })
+            .expect(200)
+            .expect(({ body }) => {
+              expect(body.data.getBoards).toBe(
+                expect.arrayContaining(['author']),
+              );
+            });
+        });
+      });
+    });
   });
 });
